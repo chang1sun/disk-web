9a5c3d4d9fc8c407503e2b9f553c1ce25c5286f5
changnoob <changnoob@outlook.com>
2022-01-22 23:14:05 +0800

    init repo

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Login from '../components/Login.vue'
 // import Home from '../components/Home.vue'
 // import Welcome from '../components/Welcome.vue'
 
